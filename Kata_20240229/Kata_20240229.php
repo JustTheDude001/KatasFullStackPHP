92054a88d93c64e18a55f8a62ac14cafd8c44bf6
muser <muser@localhost>
2024-02-29 11:01:59 +0100

    Kata20240229_Recursivity

---
+++ Kata_20240229/Kata_20240229.php
@@ -0,0 +1,87 @@
+<?php
+	
+	class Count{
+		public int $limit;
+		public int $start;
+		
+		public function __construct($start, $limit){
+			$this->start = $start;
+			$this->limit = $limit;
+		}
+		public function count($start = 0){
+			$numberToPrint = $this->start;
+			$this->start ++;
+			
+			if($this->start <= $this->limit){
+				$this->count($this->start);
+			}else{
+				echo "\n";
+			}
+			echo $numberToPrint . " ";
+		}
+	
+	}
+	
+	
+	function test_count(){
+		$countObj = new Count(0,5);
+		$countObj->count();
+		echo "\n";
+		
+		$countObj = new Count(0,3);
+		$countObj->count();
+		echo "\n";
+	}
+	
+	class Fibonacci{
+		public int $limit;
+		public int $start = 1;
+		public  $numbers = array(1);
+		public int $counter = 0;
+		
+		public function __construct($limit){
+			$this->limit = $limit;			
+		}
+		public function fibonacci($counter = 0){
+			
+			if($this->numbers[$this->counter] >= $this->limit){
+				return;
+			}
+			
+			if($this->counter < 1){
+				echo $this->numbers[0] . " ";
+				$this->counter++;
+				$this->numbers[] = $this->numbers[$this->counter - 1];
+			}else{
+				$this->counter++;
+				$this->numbers[] = $this->numbers[$this->counter - 2] + $this->numbers[$this->counter - 1];;
+			}
+			
+			echo $this->numbers[$this->counter]  . " ";
+			
+			if($this->numbers[$this->counter] <= $this->limit){
+				$this->fibonacci($this->counter);
+			}else{
+				echo "\n";
+			}
+		}
+	
+	}
+	
+	
+	function test_fibo(){
+		$countFibo= new Fibonacci(5);
+		$countFibo->fibonacci();
+		echo "\n";
+		
+		$countFibo = new Fibonacci(3);
+		$countFibo->fibonacci();
+		echo "\n";
+	}
+	
+	test_count();
+	echo "\n\n\n";
+	
+	test_fibo();
+
+?>
